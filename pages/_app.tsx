5361f3ce1d7f99d66769d00c5e3ee019b0d326d2
Stuart Clark <stuart.clark@jarden.co.nz>
2021-06-17 11:35:14 +1200

    fix:  Change meta tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,24 +2,21 @@ import { AppProps } from 'next/app'
 import '../styles/globals.css'
 import Head from 'next/head'
 import { Navbar } from '../components/NavBar'
-import GlobalProvider from '../components/GlobalProvider'
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <GlobalProvider>
-      <div>
-        <Head>
-          <title>METVUW MOBILE</title>
-          <meta name="description" content="Metvuw Mobile" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <Navbar />
-        <main className="relative">
-          <Component {...pageProps} />
-        </main>
-        <footer />
-      </div>
-    </GlobalProvider>
+    <div>
+      <Head>
+        <title>METVUW MOBILE</title>
+        <meta name="description" content="Metvuw Mobile" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+      <main className="relative">
+        <Component {...pageProps} />
+      </main>
+      <footer />
+    </div>
   )
 }
 
