0ed37a5dc44736c47314ca4c8f97cb5531311e3b
Stuart Clark <stuart.clark@jarden.co.nz>
2021-06-24 14:59:06 +1200

    feat:  Progressive Web App

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
-module.exports = {
+const withOffline = require('next-offline')
+
+const nextConfig = {
   /* config options here */
   poweredByHeader: false,
   images: {
     domains: ['www.metvuw.com', 'dpucyvo9dklo9.cloudfront.net'],
   },
 }
+
+module.exports = withOffline(nextConfig)
