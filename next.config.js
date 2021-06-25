c3f21b10213e36df6a4c4ea23ac7561008c1fca1
Stuart Clark <stuart.clark@jarden.co.nz>
2021-06-25 14:55:51 +1200

    fix: service-worker error

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ const withOffline = require('next-offline')
 
 const nextConfig = {
   /* config options here */
+  workboxOpts: {
+    swDest: '../public/service-worker.js',
+  },
+
   poweredByHeader: false,
   images: {
     domains: ['www.metvuw.com', 'dpucyvo9dklo9.cloudfront.net'],
