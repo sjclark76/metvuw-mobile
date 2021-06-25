449a53dcbaac2bb4983824b4f731e364cdafcf14
Stuart Clark <stuart.clark@jarden.co.nz>
2021-06-25 12:51:37 +1200

    fix: service-worker error

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
-const express = require('express')
-const path = require('path')
+const { createServer } = require('http')
+const { join } = require('path')
+const { parse } = require('url')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-app
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true)
+    const { pathname } = parsedUrl
+
+    // handle GET request to /service-worker.js
+    if (pathname === '/service-worker.js') {
+      const filePath = join(__dirname, '.next', pathname)
+
+      app.serveStatic(req, res, filePath)
+    } else {
+      handle(req, res, parsedUrl)
+    }
+  }).listen(3000, () => {
+    console.log(`> Ready on http://localhost:${3000}`)
+  })
+})
+/*app
   .prepare()
   .then(() => {
     const server = express()
@@ -14,7 +32,7 @@ app
     // requests to /service-worker.js
     server.get(
       '/service-worker.js',
-      express.static(path.join(__dirname, '_next'))
+      express.static(path.join(__dirname, '.next'))
     )
 
     // all other requests
@@ -29,4 +47,4 @@ app
   .catch((ex) => {
     console.error(ex.stack)
     process.exit(1)
-  })
+  })*/
