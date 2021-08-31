36748cf5bf5c062d02f73d4e32b28b0a786c142c
Stuart Clark <sjclark76@gmail.com>
2021-09-01 09:36:34 +1200

    Update server.js
    code change to trigger build

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
+  
   createServer((req, res) => {
     const parsedUrl = parse(req.url, true)
     const { pathname } = parsedUrl
