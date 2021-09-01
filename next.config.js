7a050a6dbda93080e9a013ed03edd305120bc4a2
Stuart Clark <stuart.clark@jarden.co.nz>
2021-09-01 13:44:43 +1200

    fix:  remove unsafe-eval from production

--- next.config.js
+++ next.config.js
@@ -1,32 +1,46 @@
 const withOffline = require('next-offline')
 
-const securityHeaders = [
-  {
-    key: 'X-XSS-Protection',
-    value: '1; mode=block',
-  },
-  {
-    key: 'Strict-Transport-Security',
-    value: 'max-age=63072000; includeSubDomains; preload',
-  },
-  {
-    key: 'X-DNS-Prefetch-Control',
-    value: 'on',
-  },
-  {
-    key: 'Content-Security-Policy',
-    value: "frame-ancestors 'none'",
-  },
-  {
-    key: 'Content-Security-Policy',
-    value:
-      "script-src 'self'  'sha256-QYgGfxAvFb95Wfr8/J6nXSuhYgCzYgx3g/AeaN1F5J8=' 'unsafe-eval' *.googletagmanager.com *.google-analytics.com; img-src 'self' data: https://*  *.metvuw.com",
-  },
-  {
-    key: 'X-Content-Type-Options',
-    value: 'nosniff',
-  },
-]
+const securityHeaders = function () {
+  const headers = [
+    {
+      key: 'Cache-Control',
+      value: 'max-age=31536000',
+    },
+    {
+      key: 'X-XSS-Protection',
+      value: '1; mode=block',
+    },
+    {
+      key: 'Strict-Transport-Security',
+      value: 'max-age=63072000; includeSubDomains; preload',
+    },
+    {
+      key: 'X-DNS-Prefetch-Control',
+      value: 'on',
+    },
+    {
+      key: 'Content-Security-Policy',
+      value:
+        "frame-ancestors 'none'; script-src 'self' 'sha256-QYgGfxAvFb95Wfr8/J6nXSuhYgCzYgx3g/AeaN1F5J8=' *.googletagmanager.com *.google-analytics.com; img-src 'self' data: https://*  *.metvuw.com",
+    },
+    {
+      key: 'X-Content-Type-Options',
+      value: 'nosniff',
+    },
+  ]
+
+  if (process.env.NODE_ENV !== 'production') {
+    const index = headers.findIndex(
+      (value) => value.key === 'Content-Security-Policy'
+    )
+
+    headers[index].value = headers[index].value.replace(
+      "script-src 'self'",
+      "script-src 'self' 'unsafe-eval'"
+    )
+  }
+  return headers
+}
 
 const nextConfig = {
   /* config options here */
@@ -35,7 +49,7 @@ const nextConfig = {
       {
         // Apply these headers to all routes in your application.
         source: '/(.*)',
-        headers: securityHeaders,
+        headers: securityHeaders(),
       },
     ]
   },
