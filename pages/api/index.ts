c2852f08557e629157af3b9b07190e3bbf270734
Stuart Clark <sjclark76@gmail.com>
2022-01-20 15:09:24 +1300

    build: fix build warnings

--- pages/api/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './rainChartData';
-export * from './satelliteChartData';
-export * from './satellite';
-export * from './charts/[region].spec';
-export * from './charts/[region]';
-export * from './url';
