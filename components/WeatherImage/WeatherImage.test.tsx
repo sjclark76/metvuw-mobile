4360110902fc5a735d1f2100ae8df33e866e303a
Stuart Clark <sjclark76@gmail.com>
2022-03-31 11:05:20 +1300

    test: update snapshots

--- components/WeatherImage/WeatherImage.test.tsx
+++ components/WeatherImage/WeatherImage.test.tsx
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react'
 import WeatherImage from './WeatherImage'
 
 describe('Weather Image', () => {
-  test('it should render correctl', () => {
+  test('it should render correctly', () => {
     const { container } = render(
       <WeatherImage isRainForecast imageAlt="foo" imageSrc="baa" />
     )
