99f1eba99f03282629354f0ec9d3a715a5e979d2
Stuart Clark <sjclark76@gmail.com>
2022-03-31 10:56:19 +1300

    test: add back in code coverage values

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,12 @@ module.exports = {
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   setupFilesAfterEnv: ['<rootDir>/importJestDOM.ts'],
+  coverageThreshold: {
+    global: {
+      statements: 50,
+      branches: 90,
+      functions: 0,
+      lines: 0,
+    },
+  },
 }
