c5c80831e6913c88231cea3df2d7d03d0803a35a
Stuart Clark <sjclark76@gmail.com>
2022-04-08 16:43:23 +1200

    ci: change to run e2e tests in chrome

--- cypress/integration/home-page/home.spec.ts
+++ cypress/integration/home-page/home.spec.ts
@@ -5,7 +5,7 @@ describe('home page', () => {
     // so we must tell it to visit our website with the `cy.visit()` command.
     // Since we want to visit the same URL at the start of all our tests,
     // we include it in our beforeEach function so that it runs before each test
-    cy.visit()
+    cy.visit('/')
   })
 
   it('should have a forecast issued banner', () => {
