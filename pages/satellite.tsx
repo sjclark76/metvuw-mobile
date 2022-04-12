1d04b8a4222cbc697e5a466d4eb9c8c882534a53
Stuart Clark <sjclark76@gmail.com>
2022-04-12 12:54:31 +1200

    Merge remote-tracking branch 'origin/feature/cypress' into feature/cypress
    # Conflicts:
    #	package.json

--- pages/satellite.tsx
+++ pages/satellite.tsx
@@ -55,20 +55,24 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   const apiURl = new URL(`api/satellite`, config.baseUrl)
   const response = await fetch(apiURl.href)
   console.log('retrieved response', response.status, response.statusText)
-  const satelliteImages: SatelliteChartData[] = JSON.parse(
-    '[{"year":2022,"month":3,"day":11,"hour":0,"imageDate":1649635200000,"imageDateIso":"2022-04-11T00:00:00.000Z","url":"https://www.metvuw.com/satellite/big/202204110000.jpg","width":"280","height":"210"},{"year":2022,"month":3,"day":11,"hour":3,"imageDate":1649646000000,"imageDateIso":"2022-04-11T03:00:00.000Z","url":"https://www.metvuw.com/satellite/big/202204110300.jpg","width":"280","height":"210"},{"year":2022,"month":3,"day":11,"hour":6,"imageDate":1649656800000,"imageDateIso":"2022-04-11T06:00:00.000Z","url":"https://www.metvuw.com/satellite/big/202204110600.jpg","width":"280","height":"210"},{"year":2022,"month":3,"day":11,"hour":9,"imageDate":1649667600000,"imageDateIso":"2022-04-11T09:00:00.000Z","url":"https://www.metvuw.com/satellite/big/202204110900.jpg","width":"280","height":"210"},{"year":2022,"month":3,"day":11,"hour":12,"imageDate":1649678400000,"imageDateIso":"2022-04-11T12:00:00.000Z","url":"https://www.metvuw.com/satellite/big/202204111200.jpg","width":"280","height":"210"},{"year":2022,"month":3,"day":11,"hour":15,"imageDate":1649689200000,"imageDateIso":"2022-04-11T15:00:00.000Z","url":"https://www.metvuw.com/satellite/big/202204111500.jpg","width":"280","height":"210"},{"year":2022,"month":3,"day":11,"hour":18,"imageDate":1649700000000,"imageDateIso":"2022-04-11T18:00:00.000Z","url":"https://www.metvuw.com/satellite/big/202204111800.jpg","width":"280","height":"210"},{"year":2022,"month":3,"day":11,"hour":21,"imageDate":1649710800000,"imageDateIso":"2022-04-11T21:00:00.000Z","url":"https://www.metvuw.com/satellite/big/202204112100.jpg","width":"280","height":"210"},{"year":2022,"month":3,"day":12,"hour":0,"imageDate":1649721600000,"imageDateIso":"2022-04-12T00:00:00.000Z","url":"https://www.metvuw.com/satellite/big/202204120000.jpg","width":"280","height":"210"}]'
-  ) // await response.json()
+  try {
+    const satelliteImages: SatelliteChartData[] = await response.json()
 
-  const meta = createGeneralSeoMetaProps(
-    'Satellite',
-    satelliteImages[0].url,
-    context
-  )
+    const meta = createGeneralSeoMetaProps(
+      'Satellite',
+      satelliteImages[0].url,
+      context
+    )
 
-  return {
-    props: {
-      images: satelliteImages,
-      meta,
-    }, // will be passed to the page component as props
+    return {
+      props: {
+        images: satelliteImages,
+        meta,
+      }, // will be passed to the page component as props
+    }
+  } catch (e) {
+    console.error(e)
+    console.error(response.statusText)
+    console.error(response.status)
   }
 }
