b589f1ca0e1683bbe92fb0d49375ac3dd5b156c0
Stuart Clark <sjclark76@gmail.com>
2022-05-01 11:09:36 +1200

    feat: add image cropping to weather image

--- components/WeatherImage/WeatherImage.tsx
+++ components/WeatherImage/WeatherImage.tsx
@@ -1,9 +1,15 @@
+import styles from './WeatherImage.module.css'
+
 export interface WeatherImageProps {
   imageSrc: string
   imageAlt: string
   isRainForecast: boolean
 }
-const WeatherImage = (props: WeatherImageProps) => {
+const WeatherImage = ({
+  imageAlt,
+  imageSrc,
+  isRainForecast,
+}: WeatherImageProps) => {
   const rainStyle = {
     objectPosition: '0% 62%',
     minHeight: '85%',
@@ -16,25 +22,15 @@ const WeatherImage = (props: WeatherImageProps) => {
   return (
     <div
       data-testid="weather-image"
-      className="inline-block max-w-full overflow-hidden relative box-border m-0"
+      className={isRainForecast ? styles.aspectRatioBox : null}
     >
-      <div className="box-border block, max-w-full">
-        <img
-          className="max-w-full block m-0 border-none p-0"
-          alt=""
-          aria-hidden="true"
-          role="presentation"
-          src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNzExIiBoZWlnaHQ9IjQ5MiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4="
-        />
-      </div>
-
       <img
-        loading={'lazy'}
-        alt={props.imageAlt}
-        src={props.imageSrc}
+        loading="lazy"
+        alt={imageAlt}
+        src={imageSrc}
         decoding="async"
-        className="absolute top-0 left-0 bottom-0 right-0, box-border p-0 border-none m-auto block w-0, h-0 min-w-full max-w-full  max-h-full object-cover"
-        style={props.isRainForecast ? rainStyle : satelliteStyle}
+        className={isRainForecast ? styles.croppedImage : null}
+        style={isRainForecast ? rainStyle : satelliteStyle}
       />
     </div>
   )
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,19 @@
 module.exports = {
   testEnvironment: 'jsdom',
   testRegex: '(/__tests__/.*|(\\.|/)(test))\\.tsx?$',
+  moduleNameMapper: {
+    /* Handle CSS imports (with CSS modules)
+    https://jestjs.io/docs/webpack#mocking-css-modules */
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+
+    // Handle CSS imports (without CSS modules)
+    '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+
+    // /* Handle image imports
+    // https://jestjs.io/docs/webpack#handling-static-assets */
+    // '^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$':
+    //     '<rootDir>/__mocks__/fileMock.js',
+  },
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   setupFilesAfterEnv: ['<rootDir>/importJestDOM.ts'],
   coverageThreshold: {
