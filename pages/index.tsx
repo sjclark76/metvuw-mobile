62f984cf4e69aee470ec71053b4c939278acd1e4
Stuart Clark <sjclark76@gmail.com>
2022-09-30 11:55:33 +1300

    feat: implement ISR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,12 @@
-import { GetServerSideProps } from 'next'
-import { config } from '../config'
-import {
-  createSeoMetaProps,
-  SeoMeta,
-  SeoMetaProps,
-} from '../components/SeoMeta'
-import { findRegionByCode, Region } from '../shared/region'
-import { RainChartData } from './api/types/rainChartData'
-import WeatherCharts from '../components/WeatherCharts'
-interface HomeProps {
-  region: Region
-  charts: RainChartData[]
-  meta: SeoMetaProps
-}
-export default function Home(props: HomeProps) {
-  return (
-    <>
-      <SeoMeta
-        title={props.meta.title}
-        desc={props.meta.desc}
-        url={props.meta.url}
-        imageUrl={props.meta.imageUrl}
-      />
-      <WeatherCharts charts={props.charts} region={props.region} />
-    </>
-  )
-}
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const apiURl = new URL('api/charts/nz', config.baseUrl)
-  const response = await fetch(apiURl.href)
-  const charts = await response.json()
-
-  const matchedRegion = findRegionByCode('nz')
-
-  const meta: SeoMetaProps = createSeoMetaProps(
-    matchedRegion,
-    charts[0].url,
-    context.resolvedUrl
-  )
-  return {
-    props: {
-      region: matchedRegion,
-      charts: charts,
-      meta: meta,
-    },
-  }
+export default function Home() {
+  const router = useRouter()
+  useEffect(() => {
+    const { pathname } = router
+    if (pathname === '/') {
+      router.push('/regions/nz')
+    }
+  })
 }
