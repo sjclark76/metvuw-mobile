20b136697e5ad8e0c6088a7b59cc66dbf2afa76c
Stuart Clark <stuart.clark@jarden.co.nz>
2022-10-24 10:36:08 +1300

    chore(google-analytics): update GA code

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
+        <Head title={'metvuw mobile'}>
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
@@ -18,11 +18,10 @@ class MyDocument extends Document {
             dangerouslySetInnerHTML={{
               __html: `
             window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
           `,
             }}
           />
