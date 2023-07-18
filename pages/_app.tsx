0c9e363c9a071faaf7321455f2c99bc16752d53c
Stuart Clark <stuart.clark@jarden.co.nz>
2023-07-19 08:50:54 +1200

    feat: add in vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import { AppProps } from 'next/app'
 import '../styles/globals.css'
 import Layout from '../components/Layout'
+import { Analytics } from '@vercel/analytics/react'
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   )
 }
