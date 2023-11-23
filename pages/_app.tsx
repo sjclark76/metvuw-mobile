6f3f97346e0788b0a29f40a037da8c484312296a
Stuart Clark <sjclark76@gmail.com>
2023-11-23 15:12:11 +1300

    add google analytics back in

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,30 @@ import '../styles/globals.css'
 import Layout from '../components/Layout'
 import { Analytics } from '@vercel/analytics/react'
 import { Provider } from 'jotai'
+import Script from 'next/script'
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider>
-      <Layout>
-        <Component {...pageProps} />
-        <Analytics />
-      </Layout>
-    </Provider>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      ></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {` window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+             `}
+      </Script>
+      <Provider>
+        <Layout>
+          <Component {...pageProps} />
+          <Analytics />
+        </Layout>
+      </Provider>
+    </>
   )
 }
 
