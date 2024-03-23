292dcbf1ee9800ccbd2922407bf933327f0c5e8d
Stuart Clark <sjclark76@gmail.com>
2024-03-23 21:11:31 +1300

    refactor(nextjs): add in google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import { Metadata } from 'next'
 import '../../styles/globals.css'
 import Navbar from '../components/Navbar'
+import GoogleTag from '@/components/GoogleTag'
 
 export const metadata: Metadata = {
   title: 'metvuw mobile',
-  // description: 'Welcome to Next.js',
 }
 
 export default function RootLayout({
@@ -16,6 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <GoogleTag />
       <body>
         <main className="relative">
           <Navbar />
