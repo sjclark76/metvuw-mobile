a004b62dc7cadb29193822348e628021bad81dfa
Stuart Clark <sjclark76@gmail.com>
2024-03-26 18:05:16 +1300

    test(checkly) add checkly as code

--- src/shared/types/region.ts
+++ src/shared/types/region.ts
@@ -36,16 +36,12 @@ export const regions = [
   { name: 'South Australia', code: 'saussie' },
   { name: 'South East Australia', code: 'seaussie' },
   { name: 'New Caledonia', code: 'newcaledonia' },
-  { name: 'Fiji', code: 'fiji' },
   { name: 'South West Pacific', code: 'swp' },
-  { name: 'Fiji - NZ', code: 'ocean' },
   { name: 'Europe', code: 'europe' },
   { name: 'United Kingdom', code: 'uk' },
   { name: 'Estonia', code: 'estonia' },
   { name: 'Turkey', code: 'turkey' },
   { name: 'World', code: 'world' },
-  { name: 'South Atlantic', code: 'satlantic' },
-  { name: 'North Atlantic', code: 'natlantic' },
   { name: 'USA', code: 'usa' },
   { name: 'Japan', code: 'japan' },
   { name: 'South Africa', code: 'safrica' },
@@ -87,9 +83,9 @@ export const australiaRegions: Region[] = [
 ]
 export const pacificRegions: Region[] = [
   { name: 'New Caledonia', code: 'newcaledonia' },
-  { name: 'Fiji', code: 'fiji' },
+  { code: 'fiji', name: 'Fiji Islands' },
   { name: 'South West Pacific', code: 'swp' },
-  { name: 'Fiji - NZ', code: 'ocean' },
+  // { name: 'Fiji - NZ', code: 'ocean' },
 ]
 export const europeRegions: Region[] = [
   { name: 'Europe', code: 'europe' },
