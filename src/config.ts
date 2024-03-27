04eebdf5d3b2744e1dcfe61033c11b7856b67f6c
Stuart Clark <sjclark76@gmail.com>
2024-03-28 12:56:37 +1300

    build(supabase): try and get build working

--- src/config.ts
+++ src/config.ts
@@ -10,8 +10,6 @@ export const config = {
     region: process.env.S3_REGION,
     secret: process.env.SECRET_KEY,
   },
-  supabaseServiceRoleKey:
-    process.env.SUPABASE_SERVICE_ROLE_KEY ??
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImV4cCI6MTk4MzgxMjk5Nn0.EGIM96RAZx35lJzdJsyH-qQwv8Hdp7fsn3W0YpN81IU',
-  supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL ?? 'http://127.0.0.1:54321',
+  supabaseServiceRoleKey: process.env.SUPABASE_SERVICE_ROLE_KEY ?? '',
+  supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
 }
