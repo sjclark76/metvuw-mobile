6d59206feb2c56e33d0c581db9164cee9d7e8ed7
Stuart Clark <sjclark76@gmail.com>
2024-04-01 14:16:01 +1300

    test(e2e): add e2e scraping tests

---
+++ e2e/api/scrape/upperair_api.spec.ts
@@ -0,0 +1,8 @@
+import { expect, test } from '@playwright/test'
+
+test.describe('radar api spec', () => {
+  test('requesting radar should return a 200 response', async ({ request }) => {
+    const cacheResponse = await request.get(`/api/scrape/upper-air`)
+    expect(cacheResponse.ok()).toBeTruthy()
+  })
+})
