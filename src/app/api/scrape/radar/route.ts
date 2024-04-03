2d6448d7a599a11742ae3ef49e3a81a43cd027a2
Stuart Clark <sjclark76@gmail.com>
2024-04-04 10:12:58 +1300

    chore: add extra logging

--- src/app/api/scrape/radar/route.ts
+++ src/app/api/scrape/radar/route.ts
@@ -15,18 +15,24 @@ export async function GET(_request: NextRequest) {
   const force = Boolean(_request.nextUrl.searchParams.get('force'))
 
   const newImages = await scrapeRadarImages()
-
+  console.log('scrapeRadarImages completed')
   const existingImages = await retrieveImagesFromStorage('radar')
+
+  console.log('retrieveImagesFromStorage completed')
 
   const imagesToAdd = force
     ? newImages
     : determineImagesToAdd(newImages, existingImages)
 
+  console.log('images to add', { imagesToAdd })
+
   if (imagesToAdd.length > 0 || force) {
     await removeImagesFromStorage(existingImages)
+    console.log('removeImagesFromStorage completed')
   }
 
   const result = await uploadImagesToStorage(imagesToAdd, compressRadarImage)
+  console.log('uploadImagesToStorage completed')
 
   return NextResponse.json(result)
 }
