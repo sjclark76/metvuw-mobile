2885a47b27bdb8965d0d6aed4070d7b61cae477c
Stuart Clark <sjclark76@gmail.com>
2024-04-05 12:18:52 +1300

    fix: set dimensions on skeleton (#978)
    * fix: set dimensions on skeleton
    
    * fix: build

--- src/app/regions/[name]/loading.tsx
+++ src/app/regions/[name]/loading.tsx
@@ -1,11 +1,21 @@
 import { WeatherChartSkeleton } from '@/components/WeatherCharts/Skeleton'
+import { rainImageDimensions } from '@/shared/helpers/v2/imageCompression/imageDimensions'
 
 export default function Loading() {
   return (
     <ul className="flex flex-col items-center">
-      <WeatherChartSkeleton />
-      <WeatherChartSkeleton />
-      <WeatherChartSkeleton />
+      <WeatherChartSkeleton
+        width={rainImageDimensions.width}
+        height={rainImageDimensions.height}
+      />
+      <WeatherChartSkeleton
+        width={rainImageDimensions.width}
+        height={rainImageDimensions.height}
+      />
+      <WeatherChartSkeleton
+        width={rainImageDimensions.width}
+        height={rainImageDimensions.height}
+      />
     </ul>
   )
 }
--- src/app/satellite/loading.tsx
+++ src/app/satellite/loading.tsx
@@ -1,13 +1,23 @@
 // import { WeatherChartSkeleton } from '@/components/WeatherCharts/Skeleton'
 
 import { WeatherChartSkeleton } from '@/components/WeatherCharts/Skeleton'
+import { satelliteImageDimensions } from '@/shared/helpers/v2/imageCompression/imageDimensions'
 
 export default function Loading() {
   return (
     <ul className="flex flex-col items-center">
-      <WeatherChartSkeleton />
-      <WeatherChartSkeleton />
-      <WeatherChartSkeleton />
+      <WeatherChartSkeleton
+        width={satelliteImageDimensions.width}
+        height={satelliteImageDimensions.height}
+      />
+      <WeatherChartSkeleton
+        width={satelliteImageDimensions.width}
+        height={satelliteImageDimensions.height}
+      />
+      <WeatherChartSkeleton
+        width={satelliteImageDimensions.width}
+        height={satelliteImageDimensions.height}
+      />
     </ul>
   )
 }
