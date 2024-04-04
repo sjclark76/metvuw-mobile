4f7e880eaeee26f7f393af241c9fe0ec98a960d3
Stuart Clark <sjclark76@gmail.com>
2024-04-05 12:45:25 +1300

    style: add in loading messages (#979)

--- src/app/radar/[code]/loading.tsx
+++ src/app/radar/[code]/loading.tsx
@@ -1,21 +1,25 @@
+import SubHeader from '@/components/SubHeader'
 import { WeatherChartSkeleton } from '@/components/WeatherCharts/Skeleton'
 import { radarImageDimensions } from '@/shared/helpers/v2/imageCompression/imageDimensions'
 
 export default function Loading() {
   return (
-    <ul className="flex flex-col items-center">
-      <WeatherChartSkeleton
-        width={radarImageDimensions.width}
-        height={radarImageDimensions.height}
-      />
-      <WeatherChartSkeleton
-        width={radarImageDimensions.width}
-        height={radarImageDimensions.height}
-      />
-      <WeatherChartSkeleton
-        width={radarImageDimensions.width}
-        height={radarImageDimensions.height}
-      />
-    </ul>
+    <>
+      <SubHeader submenuText="Loading radar data" />
+      <ul className="flex flex-col items-center">
+        <WeatherChartSkeleton
+          width={radarImageDimensions.width}
+          height={radarImageDimensions.height}
+        />
+        <WeatherChartSkeleton
+          width={radarImageDimensions.width}
+          height={radarImageDimensions.height}
+        />
+        <WeatherChartSkeleton
+          width={radarImageDimensions.width}
+          height={radarImageDimensions.height}
+        />
+      </ul>
+    </>
   )
 }
