27d945dab4d1c0f98f4c09a95f84719e36d15880
Stuart Clark <sjclark76@gmail.com>
2024-04-05 12:35:42 +1300

    style: add in loading messages

--- src/app/satellite/loading.tsx
+++ src/app/satellite/loading.tsx
@@ -1,23 +1,27 @@
 // import { WeatherChartSkeleton } from '@/components/WeatherCharts/Skeleton'
 
+import SubHeader from '@/components/SubHeader'
 import { WeatherChartSkeleton } from '@/components/WeatherCharts/Skeleton'
 import { satelliteImageDimensions } from '@/shared/helpers/v2/imageCompression/imageDimensions'
 
 export default function Loading() {
   return (
-    <ul className="flex flex-col items-center">
-      <WeatherChartSkeleton
-        width={satelliteImageDimensions.width}
-        height={satelliteImageDimensions.height}
-      />
-      <WeatherChartSkeleton
-        width={satelliteImageDimensions.width}
-        height={satelliteImageDimensions.height}
-      />
-      <WeatherChartSkeleton
-        width={satelliteImageDimensions.width}
-        height={satelliteImageDimensions.height}
-      />
-    </ul>
+    <>
+      <SubHeader submenuText="Loading satellite data" />
+      <ul className="flex flex-col items-center">
+        <WeatherChartSkeleton
+          width={satelliteImageDimensions.width}
+          height={satelliteImageDimensions.height}
+        />
+        <WeatherChartSkeleton
+          width={satelliteImageDimensions.width}
+          height={satelliteImageDimensions.height}
+        />
+        <WeatherChartSkeleton
+          width={satelliteImageDimensions.width}
+          height={satelliteImageDimensions.height}
+        />
+      </ul>
+    </>
   )
 }
