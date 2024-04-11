1b38082644f0a6ad2562ba0536da37b103d990d8
Stuart Clark <sjclark76@gmail.com>
2024-04-11 15:31:09 +1200

    fix:add remove-image api

---
+++ src/app/api/remove-images/route.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+import { removeImage } from '@/shared/helpers/v2/jobs'
+
+export async function GET(_request: NextRequest) {
+  const numberParam = _request.nextUrl.searchParams.get('number')
+
+  const number = numberParam ? +numberParam : 1
+  const removeCount = await removeImage(number)
+
+  return NextResponse.json({
+    ok: true,
+    'number-requested': number,
+    'number-removed': removeCount,
+  })
+}
--- src/shared/helpers/v2/jobs/removeImagesJob.ts
+++ src/shared/helpers/v2/jobs/removeImagesJob.ts
@@ -39,3 +39,37 @@ export async function removeImagesJob(id: number, triggerKey: string) {
     }
   }
 }
+
+export async function removeImage(number: number) {
+  const { data: images } = await serviceRoleDb
+    .from('images_to_remove')
+    .select('*')
+    .eq('bucket_id', config.supbabaseBucketName)
+    .select()
+    .limit(number)
+
+  if (images != null) {
+    const imagePaths = images.map((img) => img.full_storage_path)
+
+    const { error } = await serviceRoleDb.storage
+      .from(config.supbabaseBucketName)
+      .remove(imagePaths)
+
+    if (error) {
+      console.error(error)
+    }
+
+    const imageKeys = images.map((img) => img.trigger_key)
+    const { error: deleteError } = await serviceRoleDb
+      .from('images_to_remove')
+      .delete()
+      .eq('bucket_id', config.supbabaseBucketName)
+      .eq('trigger_key', imageKeys)
+
+    if (deleteError) {
+      console.error(deleteError)
+    }
+
+    return images.length
+  }
+}
