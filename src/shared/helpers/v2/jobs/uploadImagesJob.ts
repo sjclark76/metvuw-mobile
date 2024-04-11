93cf07ab13420e7c8826062b298ff499b74eb099
Stuart Clark <sjclark76@gmail.com>
2024-04-11 16:05:33 +1200

    fix:add manual upload

--- src/shared/helpers/v2/jobs/uploadImagesJob.ts
+++ src/shared/helpers/v2/jobs/uploadImagesJob.ts
@@ -74,3 +74,63 @@ export async function uploadImagesJob(id: number, triggerKey: string) {
     }
   }
 }
+
+export async function uploadImages(number: number) {
+  const { data: images } = await serviceRoleDb
+    .from('images_to_upload')
+    .select('*')
+    .eq('bucket_id', config.supbabaseBucketName)
+    .limit(number)
+    .select()
+
+  if (images != null) {
+    try {
+      await Promise.all(
+        images.map(
+          async ({
+            chart_type,
+            original_image_url,
+            full_storage_path,
+            small_image_storagePath,
+          }) => {
+            const image = await downloadImageToBuffer(original_image_url)
+
+            const { primary, small } = getCompressorForChart(
+              chart_type as ChartType,
+            )
+
+            const imageToUpload = await primary(image.fileBuffer)
+
+            if (small && small_image_storagePath) {
+              const smallImage = await small(image.fileBuffer)
+              await uploadImage(
+                config.supbabaseBucketName,
+                small_image_storagePath,
+                smallImage,
+              )
+            }
+            return uploadImage(
+              config.supbabaseBucketName,
+              full_storage_path,
+              imageToUpload,
+            )
+          },
+        ),
+      )
+    } catch (e) {
+      console.error('error uploading image', e)
+    } finally {
+      const imageKeys = images.map((img) => img.trigger_key)
+      const { error: deleteError } = await serviceRoleDb
+        .from('images_to_upload')
+        .delete()
+        .eq('bucket_id', config.supbabaseBucketName)
+        .in('trigger_key', imageKeys)
+
+      if (deleteError) {
+        console.error(deleteError)
+      }
+    }
+  }
+  return images?.length
+}
