494dd84e64b4def01d29da0fedbfb4e321759c2c
Stuart Clark <sjclark76@gmail.com>
2024-04-12 17:51:55 +1200

    refactor(remove old code):

--- src/shared/helpers/generateTriggerCode.ts
+++
@@ -1,5 +0,0 @@
-import { config } from '@/config'
-
-const generateTriggerCode = (name: string) =>
-  `${config.supbabaseBucketName}-${name}-${Date.now()}`
-export default generateTriggerCode
--- src/shared/helpers/v2/imageStorage/uploadImagesToStorage.ts
+++ src/shared/helpers/v2/imageStorage/uploadImagesToStorage.ts
@@ -1,13 +1,6 @@
-/* eslint-disable no-unused-vars */
-// eslint-disable-next-line no-unused-vars
 import path from 'node:path'
 
-import * as buffer from 'buffer'
-
-import { config } from '@/config'
 import serviceRoleDb from '@/shared/db/serviceRoleDb'
-import { downloadImageToBuffer } from '@/shared/helpers/v2/imageStorage/downloadImageToBuffer'
-import { ScrapedImage } from '@/shared/helpers/v2/screenScraper/scrapedImage'
 
 export function uploadImage(
   bucketId: string,
@@ -22,36 +15,4 @@ export function uploadImage(
       contentType: 'image/webp',
       upsert: false,
     })
-}
-
-export async function uploadImagesToStorage(
-  imagesToUpload: ScrapedImage[],
-  transformer?: (i: buffer.Buffer) => Promise<buffer.Buffer>,
-  thumbnailTransformer?: (i: buffer.Buffer) => Promise<buffer.Buffer>,
-) {
-  return Promise.all(
-    imagesToUpload.map(
-      async ({ originalImageURL, fullStoragePath, smallImageStoragePath }) => {
-        const image = await downloadImageToBuffer(originalImageURL)
-
-        const imageToUpload = transformer
-          ? await transformer(image.fileBuffer)
-          : image.fileBuffer
-
-        if (thumbnailTransformer && smallImageStoragePath) {
-          const smallImage = await thumbnailTransformer(image.fileBuffer)
-          await uploadImage(
-            config.supbabaseBucketName,
-            smallImageStoragePath,
-            smallImage,
-          )
-        }
-        return uploadImage(
-          config.supbabaseBucketName,
-          fullStoragePath,
-          imageToUpload,
-        )
-      },
-    ),
-  )
 }
