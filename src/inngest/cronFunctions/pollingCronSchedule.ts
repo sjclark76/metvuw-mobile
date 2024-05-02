d703e5f34a2597f29f4ecdc02ce850484becf520
Stuart Clark <sjclark76@gmail.com>
2024-05-03 08:20:58 +1200

    refactor: change default poller time to every six hours

--- src/inngest/cronFunctions/pollingCronSchedule.ts
+++ src/inngest/cronFunctions/pollingCronSchedule.ts
@@ -3,8 +3,8 @@ import { config } from '@/config'
 export const everyHalfHour = { cron: '*/30 * * * *' }
 export const everyTwoHours = { cron: '0 */2 * * *' }
 
-export const everyEightHours = { cron: '0 */8 * * *' }
+export const everySixtHours = { cron: '0 */6 * * *' }
 export const never = { cron: '59 23 31 12 * 1970' }
 
 export const defaultPollerTime =
-  config.environment === 'preview' ? never : everyEightHours
+  config.environment === 'preview' ? never : everySixtHours
