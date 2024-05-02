f82df6449dc3d6ccbda29c88f6dc0f8746bfb0b2
Stuart Clark <sjclark76@gmail.com>
2024-05-02 12:51:29 +1200

    refactor: change default poller time for preview to never

--- src/inngest/cronFunctions/pollingCronSchedule.ts
+++ src/inngest/cronFunctions/pollingCronSchedule.ts
@@ -1,6 +1,10 @@
+import { config } from '@/config'
+
 export const everyHalfHour = { cron: '*/30 * * * *' }
 export const everyTwoHours = { cron: '0 */2 * * *' }
 
 export const everyEightHours = { cron: '0 */8 * * *' }
+export const never = { cron: '59 23 31 12 * 1970' }
 
-export const defaultPollerTime = everyEightHours
+export const defaultPollerTime =
+  config.environment === 'preview' ? never : everyEightHours
