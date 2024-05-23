f199435d11d8d4d904a37dfb919d2ad9108472e4
Stuart Clark <sjclark76@gmail.com>
2024-05-23 14:10:08 +1200

    fix: playwright tests

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
+import { Metadata } from 'next'
 import { notFound } from 'next/navigation'
 
 import HomePage from '@/components/HomePage'
 import NoForecast from '@/components/NoForecast'
+import generateSEOMetadata from '@/shared/helpers/generateSEOMetadata'
 import { constructRainChartData } from '@/shared/helpers/v2/chartData/constructRainChartData'
 import { retrieveImagesFromStorage } from '@/shared/helpers/v2/imageStorage'
 import { findRegionByCode } from '@/shared/types/region'
 
 export const dynamic = 'force-dynamic'
+
+export async function generateMetadata(): Promise<Metadata> {
+  // read route params
+  const regionName = 'nz'
+  return generateSEOMetadata({
+    title: `metvuw mobile | ${regionName}`,
+    description: `metvuw ${regionName} wind & rain forecast charts. Optimized for mobile devices. Sourced from metvuw.com`,
+    url: `regions/${regionName}`,
+  })
+}
 
 export default async function Page() {
   const matchedRegion = findRegionByCode('nz')
