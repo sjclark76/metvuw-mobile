c63a1f012c943640318843c40be947acc870dc2e
stuartclark <stuart.clark@educationperfect.com>
2025-06-09 20:28:30 +1200

    perf: pre load images for all pages

--- src/components/UpperAirPage/UpperAirPage.tsx
+++ src/components/UpperAirPage/UpperAirPage.tsx
@@ -1,4 +1,3 @@
-import { usePreloadedImages } from '@/components/Hooks/usePreloadedImages'
 import RadarAndSatelliteImages from '@/components/RadarAndSatelliteImages'
 import SubHeader from '@/components/SubHeader'
 import { SkinnyChartData } from '@/shared/helpers/v2/chartData/constructChartData'
@@ -11,7 +10,6 @@ export function UpperAirPage({
   chartData: SkinnyChartData[]
   balloonCode: string
 }) {
-  usePreloadedImages(chartData)
   return (
     <>
       <SubHeader
