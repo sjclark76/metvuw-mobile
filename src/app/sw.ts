19b4478b249ab571ff0ee0dc60fd5fcfd647d9da
Stuart Clark <stuart.clark@operandio.com>
2025-07-12 20:36:10 +1200

    feat: tidy up

--- src/app/sw.ts
+++ src/app/sw.ts
@@ -5,8 +5,7 @@ import { Serwist } from 'serwist'
 // This declares the value of `self.__SW_MANIFEST`.
 // See https://serwist.pages.dev/docs/next/configuring
 declare global {
-  // eslint-disable-next-line no-unused-vars
-  interface WorkerGlobalScope extends SerwistGlobalConfig {
+  interface ServiceWorkerGlobalScope extends SerwistGlobalConfig {
     __SW_MANIFEST: (PrecacheEntry | string)[] | undefined
   }
 }
@@ -35,41 +34,9 @@ const serwist = new Serwist({
   clientsClaim: true,
   navigationPreload: false,
   runtimeCaching: defaultCache,
-  // runtimeCaching: [
-  //   // {
-  //   //   matcher({ request }) {
-  //   //     return request.mode === 'navigate'
-  //   //   },
-  //   //   handler: new NetworkFirst({
-  //   //     cacheName: 'pages',
-  //   //     matchOptions: {
-  //   //       ignoreSearch: true,
-  //   //     },
-  //   //   }),
-  //   // },
-  //   ...defaultCache,
-  // ],
 })
 //
 serwist.setCatchHandler(async ({ request }) => {
-  // eslint-disable-next-line no-console
-  console.log('Service Worker:', request)
-  // const url = new URL(request.url)
-  // url.search = '' // Normalize URL by removing search parameters
-  //
-  // if (
-  //   request.destination === 'document' ||
-  //   request.destination === '' ||
-  //   request.destination === 'script'
-  // ) {
-  //   const cachedResponse = await caches.match(url.toString(), {
-  //     ignoreSearch: true,
-  //   })
-  //   if (cachedResponse) {
-  //     return cachedResponse
-  //   }
-  // }
-  //
   // Fallback for document requests to the offline page
   if (request.destination === 'document') {
     const offlinePage = await caches.match('/offline')
