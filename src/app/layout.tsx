dca2d514d6239936d1bae7bc6d8d124724229c69
Stuart Clark <sjclark76@gmail.com>
2025-07-13 14:51:15 +1200

    style: cache busting test (#1644)
    Co-authored-by: Stuart Clark <stuart.clark@operandio.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@
 import './globals.css'
 
 import { Provider } from 'jotai'
-import React, { useEffect } from 'react'
-import { toast, Toaster } from 'react-hot-toast'
+import React from 'react'
+import { Toaster } from 'react-hot-toast'
 
 import GoogleTag from '@/components/GoogleTag'
 import { config } from '@/config'
@@ -15,20 +15,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      const handleMessage = (event: MessageEvent) => {
-        if (event.data && event.data.type === 'OFFLINE') {
-          toast.error('You are offline. Showing cached page.')
-        }
-      }
-      navigator.serviceWorker.addEventListener('message', handleMessage)
-      return () => {
-        navigator.serviceWorker.removeEventListener('message', handleMessage)
-      }
-    }
-  }, [])
-
   return (
     <html lang="en" className="bg-gray-100">
       <head>
@@ -41,7 +27,7 @@ export default function RootLayout({
         <Toaster />
         <div className="font-sans text-gray-800">
           <Provider>
-            <header className="sticky top-0 z-30 bg-white shadow-md">
+            <header className="sticky top-0 z-30 border-b-4 border-red-500 bg-white shadow-md">
               <Navbar />
             </header>
             <main className="mx-auto dark:bg-stone-600 dark:text-stone-100">
