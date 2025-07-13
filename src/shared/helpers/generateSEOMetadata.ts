3ee42ca87c3edb1bad33890ce43ab6bc4af8b7b8
Stuart Clark <stuart.clark@operandio.com>
2025-07-13 15:33:21 +1200

    feat: Add iOS PWA installation support
    Implements "Add to Home Screen" functionality for iOS devices by:
    
    - Adding necessary `apple-web-app-capable` and splash screen metadata to the `generateSEOMetadata` helper.
    - Creating a new `InstallPWA` component that displays an informational banner for iOS users, guiding them through the manual installation process via the Share menu.

--- src/shared/helpers/generateSEOMetadata.ts
+++ src/shared/helpers/generateSEOMetadata.ts
@@ -78,6 +78,63 @@ export default function generateSEOMetadata({
     ],
     manifest: '/manifest.json',
     metadataBase: new URL(config.baseUrl),
+    appleWebApp: {
+      capable: true,
+      title: 'Metvuw Mobile',
+      statusBarStyle: 'default',
+      startupImage: [
+        {
+          url: '/placeholder.png',
+          media:
+            '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)',
+        },
+        {
+          url: '/placeholder.png',
+          media:
+            '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)',
+        },
+        {
+          url: '/placeholder.png',
+          media:
+            '(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)',
+        },
+        {
+          url: '/placeholder.png',
+          media:
+            '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)',
+        },
+        {
+          url: '/placeholder.png',
+          media:
+            '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)',
+        },
+        {
+          url: '/placeholder.png',
+          media:
+            '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)',
+        },
+        {
+          url: '/placeholder.png',
+          media:
+            '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)',
+        },
+        {
+          url: '/placeholder.png',
+          media:
+            '(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)',
+        },
+        {
+          url: '/placeholder.png',
+          media:
+            '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)',
+        },
+        {
+          url: '/placeholder.png',
+          media:
+            '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)',
+        },
+      ],
+    },
     openGraph: {
       description,
       siteName: 'Metvuw Mobile',
