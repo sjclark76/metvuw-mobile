aa05dd7673e60e6ccd659ad041d40269e9299681
Stuart Clark <stuart.clark@operandio.com>
2025-07-26 10:36:37 +1200

    style: tidy up

--- src/components/RefreshButton/RefreshButton.tsx
+++ src/components/RefreshButton/RefreshButton.tsx
@@ -17,7 +17,7 @@ export const RefreshButton = () => {
   return (
     <button
       onClick={handleRefresh}
-      className="hidden h-9 w-9 items-center justify-center rounded-full bg-gray-100 text-gray-600 shadow-sm hover:bg-gray-200 focus:outline-none dark:bg-stone-600 dark:text-stone-200 dark:hover:bg-stone-500 md:flex"
+      className="hidden h-9 w-9 items-center justify-center rounded-full bg-gray-100 text-gray-600 shadow-sm hover:bg-gray-200 focus:outline-none md:flex dark:bg-stone-600 dark:text-stone-200 dark:hover:bg-stone-500"
       aria-label="Refresh page"
     >
       <RefreshIcon className="h-6 w-6" />
